ed9b6cb77aaaeb8365b2264c073dc9bb7c4628d6
Hasan Rahman <hasanrahman33605@gmail.com>
2025-03-19 23:55:47 +0600

    ..

--- script.js
+++ script.js
@@ -16,4 +16,42 @@ async function getYoutubeVideos() {
   }
 }
 
+//display video
+function displayVideo(videos) {
+  const videoContainer = document.getElementById("videoContainer");
+  videoContainer.innerHTML = "";
+
+  videos.forEach((video) => {
+    const snippet = video.items.snippet;
+    const videoId = video.items?.id;
+    if (!snippet || !videoId) return;
+
+    const { title, channelTitle, thumbnails } = snippet;
+    const { url: thumbnailUrl } = thumbnails.high;
+
+    const videoCard = document.createElement("div");
+    videoCard.className =
+      "w-80 bg-white shadow-lg rounded-lg overflow-hidden cursor-pointer";
+    videoCard.innerHTML = `
+                    <div class="relative">
+                        <img class="w-full h-44 object-cover" src="${thumbnailUrl}" alt="Thumbnail">
+                    </div>
+                    <div class="p-3 flex space-x-3">
+                        <img class="w-10 h-10 rounded-full" src="https://via.placeholder.com/40" alt="Channel Avatar">
+                        <div>
+                            <h3 class="text-sm font-semibold text-gray-900 leading-tight">${title}</h3>
+                            <p class="text-xs text-gray-500">${channelTitle}</p>
+                        </div>
+                    </div>
+      `;
+
+    //open youtube when click video
+    videoCard.addEventListener("click", function () {
+      window.open(`https://www.youtube.com/watch?v=${videoId}`, "_blank");
+    });
+
+    videoContainer.append(videoCard);
+  });
+}
+
 getYoutubeVideos();
